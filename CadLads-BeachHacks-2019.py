dddd83e3958b7965edd6b945f3481caa68ed0954
Michael-Lopez2816 <49824816+Michael-Lopez2816@users.noreply.github.com>
2019-04-21 11:25:24 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -7,6 +7,6 @@ Person: 1 I am Alexis, your commander in chief my friend.
   We knew each other from the fights of Ren against the MIGHTY Rat the Revenger.
   You were knocked during the fight and was transfered over to the medical center to recive treatment.
   Person 2: Medical Center! Rat the Revenger! He will pay for what he did to my people!
-  Person 1: I will be guding you through this dungeon to slay these creatures.
-  They have conquered the lands of paradise for centuries and have taken over the pentagon.
-  We need your help into fighting these creatures of Rat the Revenger as we continue to loose food supplies. 
+  Person 1: Our people, they suffered in the hands of Rat the Revenger will be guding you through this dungeon to slay these creatures.
+  They have conquered the lands of paradise for centuries and have taken over the Kingdom of Vandamanium.
+If not he's going to destroy the Vandamanium's food supply and shelter locations. 
