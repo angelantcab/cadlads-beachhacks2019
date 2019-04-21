98b04a5c0a56e72493ae30987a96f827137afb11
Michael-Lopez2816 <49824816+Michael-Lopez2816@users.noreply.github.com>
2019-04-21 11:14:31 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -1,1 +1,12 @@
-
+Person: 1 Whats your name?
+What do you remember about your past life?
+How did you get inside this dungeon?
+Person 2: Who in god's name are you?
+ What do you want from me?
+Person: 1 I am Alexis, your commander in chief my friend.
+  We knew each other from the fights of Ren against the MIGHTY Rat the Revenger.
+  You were knocked during the fight and was transfered over to the medical center to recive treatment.
+  Person 2: Medical Center! Rat the Revenger! He will pay for what he did to my people!
+  I will be guding you through this dungeon to slay these creatures.
+  They have conquered the lands of paradise for centuries and have taken over the pentagon.
+  We need your help into fighting these creatures as they continue 
