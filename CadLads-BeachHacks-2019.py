2a236c2a4b9bcb418d7a63fb33ad32bfeea3fdd3
Michael-Lopez2816 <49824816+Michael-Lopez2816@users.noreply.github.com>
2019-04-21 11:37:40 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -1,12 +1,14 @@
 Person: 1 Whats your name?
 What do you remember about your past life?
-How did you get inside this dungeon?
-Person 2: Who in god's name are you?
- What do you want from me?
-Person: 1 I am Alexis, your commander in chief my friend.
-  We knew each other from the fights of Ren against the MIGHTY Rat the Revenger.
-  You were knocked during the fight and was transfered over to the medical center to recive treatment.
-  Person 2: Medical Center! Rat the Revenger! He will pay for what he did to my people!
-  Person 1: Our people, they suffered in the hands of Rat the Revenger will be guding you through this dungeon to slay these creatures.
-  They have conquered the lands of paradise for centuries and have taken over the Kingdom of Vandamanium.
+What do you remember about the fight?
+Person 3: What fight? What fight? Tell me what fight?
+ Person: 1 Relax, Relax, I see, this test subject is experiencing a case of Amnesia. If we manipulate his development of the prefrontal lobe 
+  where memory is stored, he can listen to our commands and be possessed forever!mwhahhahahaha
+  Person: 2 Mwhhahahahahha!
+  Person 1 We knew each other from the fights of Ren against the MIGHTY Rat the Revenger.
+  You were knocked during the fight and was transfered over to the medical center to receive treatment.
+  Person 3: Medical Center! Rat the Revenger! He will pay for what he did to my people!
+  Person 1: Our people, they suffered in the hands of Rat the Revenger, he ate them, he multilated their bodies for his henchmen.
+  They have conquered the lands of paradise for centuries and have taken over the Kingdom of Vandamanium!!!!
+  The kingdom of Vandamanium 
 If not he's going to destroy the Vandamanium's food supply and shelter locations. 
