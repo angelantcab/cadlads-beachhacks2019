cae846dace3fd2692092b20781e0f252aa183f65
Angel A. Cabral <angelantcab@gmail.com>
2019-04-21 10:49:53 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -14,14 +14,20 @@ def newline():
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-dungeon = [[room, room, room], [room, room, room], [room, room, room],
-           [room, room, room], [room, room, room], [room, room, room],
-           [room, room, room], [room, room, room], [room, room, room]]
+dungeon = [[0, 0, 0], [0, 0, 0], [0, 0, 0],
+           [0, 0, 0], [0, 0, 0], [0, 0, 0],
+           [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+player_hp = 100
+player_ad = 25
+
+enemy_hp = 80
+enemy_ad = 20
 
 def dungeon_enemies(map):
     for corridor in map:
         for room in corridor:
-            room = random.randint(0 ,1)
+            room = random.randint(0, 1)
 
 def get_player_name():
     name = input("What is your name? ")
@@ -33,13 +39,24 @@ def is_conflict(conflict):
     return 1
 
 def print_battle_options():
-    print("Fight Options\n\n1. Attack\n2. Bag\n3. Run")
+    enemy_name = ["Goblin", "Golem", "Troll", "Giant Rat",
+                  "Dragon", "Imp"]
+    choose_enemy = random.randint(0, 5)
+    print("You are met with a {0}!\nEnemy Health = {1}\n"
+          "Enemy Damage = {2}\n\nBattle Options:\n1. Attack\n2. "
+          "Bag\n3. Run".format(enemy_name[choose_enemy],
+                                enemy_hp, enemy_ad))
+
+print_battle_options()
 
 def get_battle_option():
     choice = input("Enter your choice: ")
     return choice
 
+'''
 def battle():
     print_battle_options()
     choice = get_battle_option():
     if choice == 1:
+        if
+'''
