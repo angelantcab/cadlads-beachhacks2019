b86390f49ae5f797389997ccf311cc559dcbdb30
Angel A. Cabral <angelantcab@gmail.com>
2019-04-21 10:53:59 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -1,13 +1,1 @@
-# Cabral, Angel
-# Docuyanan, Dillon
-# Lopez, Michael
-# 4/20/19
 
-# CadLads - BeachHacks 2019
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-def newline():
-    print("\n----------------------------")
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
