7b4c4e7a6e724f1c33fe2c91485d47837bf20ffb
Angel A. Cabral <angelantcab@gmail.com>
2019-04-20 21:37:56 -0700

    Update CadLads-BeachHacks-2019.py

--- CadLads-BeachHacks-2019.py
+++ CadLads-BeachHacks-2019.py
@@ -14,18 +14,30 @@ def newline():
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-maze = [
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]
-       ["|----------|"]]
+room = random.randint(0, 1)
 
-player = "^"
+dungeon = [[room, room, room], [room, room, room], [room, room, room],
+           [room, room, room], [room, room, room], [room, room, room],
+           [room, room, room], [room, room, room], [room, room, room]]
+
+def get_player_name():
+    name = input("What is your name? ")
+    return name
+
+def is_conflict(conflict):
+    if conflict == 0:
+        return 0
+    return 1
+
+def print_battle_options():
+    print("Fight Options\n\n1. Attack\n2. Bag\n3. Run")
+
+def get_battle_option():
+    choice = input("Enter your choice: ")
+    return choice
+
+def battle():
+    print_battle_options()
+    choice = get_battle_option():
+    if choice == 1:
+        
